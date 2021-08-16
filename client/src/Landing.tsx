e6c2c016d4650a86fd2b2687499df1d50b708665
Conrad Olega <conradolega@gmail.com>
2021-08-16 21:25:47 +0800

    Create landing page component

---
+++ client/src/Landing.tsx
@@ -0,0 +1,12 @@
+const Landing = () => {
+  return (
+    <div
+      className="w-full h-screen flex flex-col items-center justify-center bg-gray-400">
+      <p className="bg-black text-center text-white p-2 my-2 lg:text-4xl">Coming soon!</p>
+      <p className="bg-black text-center text-white p-2 my-2 lg:text-2xl">Join us on Discord for setlists, future events, and more music discussion!</p>
+      <p className="bg-black text-center text-white p-2 my-2 lg:text-4xl underline"><a href="https://discord.gg/JADPcZUDhC">Click here</a></p>
+    </div>              
+  );
+}
+
+export default Landing;
